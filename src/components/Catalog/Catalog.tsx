0964d8865a037f95ffe156c9bd4ff7a1fa8f1133
Evgexa70001 <58167315+Evgexa70001@users.noreply.github.com>
2024-12-11 09:16:43 +0300

    Update Catalog.tsx

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -698,7 +698,7 @@ const Catalog = () => {
 					</div>
 				) : (
 					<div
-						className='products-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'
+						className='products-grid grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8'
 						role='region'
 						aria-label='Список товаров'
 					>
